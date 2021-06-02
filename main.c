380562e46679680db5a386a34242a29ae73cac9f
Nathan Galmiche <nathan.galmiche@sfr.fr>
2021-06-02 10:50:12 +0200

    TD3.1

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+int recu(int prem, int deu,int n);
+int fiboRec(int n);
+
 int main(){
-    int a =5;
-    printf( "Hello World!\n");
-    printf( "a=%d\n",a);
+    printf("%d\n",fiboRec(8));
+}
+
+int fiboRec(int n){
+    if (n<3){
+        return n-1;
+    }
+    n=n-3;
+    return recu(0,1,n);
+}
+int recu(int prem, int deu,int n){
+    if(!n){
+        return prem+deu;
+    }
+    n--;
+    deu=prem+deu;
+    prem=deu-prem;
+    return recu(prem,deu,n);
 }
